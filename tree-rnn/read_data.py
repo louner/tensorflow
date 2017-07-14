d18a39aeb33c2b012cb9397d92d999866c03c232
louner <lounera@gmail.com>
2017-07-15 07:55:00 +0800

    read data using file queue

--- tree-rnn/read_data.py
+++ tree-rnn/read_data.py
@@ -1,30 +1,31 @@
 import tensorflow as tf
 import json
 
-file_queue = tf.train.string_input_producer(['data.json'])
-
 def read(file_queue):
     reader = tf.TextLineReader()
     key, value = reader.read(file_queue)
     return [value]
 
-with tf.Session() as sess:
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-    value = read(file_queue)
-
-    print(sess.run(value))
-
+def make_batch(file_queue):
     readers = [read(file_queue) for _ in range(5)]
-    batch_size = 2
-    capacity = 5
-    min_after_dequeue = 2
+    batch_size = 10000
+    min_after_dequeue = batch_size
+    capacity = min_after_dequeue*3 + batch_size
 
     value_batch = tf.train.shuffle_batch_join(readers,
                                 batch_size=batch_size,
                                 capacity=capacity,
                                 min_after_dequeue=min_after_dequeue)
 
-    print(value_batch)
-    print(sess.run(value_batch))
+    return value_batch
 
+if __name__ == '__main__':
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        '''
+        value = read(file_queue)
+        print(sess.run(value))
+        '''
+        value = make_batch(file_queue)
+        print(sess.run(value))
